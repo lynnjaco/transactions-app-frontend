e96747e8d643ec0eb9103dacc4069645309cfc2f
lynnjaco <jacobs.lynnette@gmail.com>
2024-07-05 00:29:55 -0400

    adds BrowserRouter to App and base router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Home/> }/>
+        </Routes>
+      </Router>
     </>
   )
 }
