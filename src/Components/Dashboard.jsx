7a78615b87194784fa70f31eaf81a189dcda9cc5
lynnjaco <jacobs.lynnette@gmail.com>
2024-07-06 16:35:49 -0400

    adds container styling to the dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,9 +1,24 @@
 import './Dashboard.css'
 
+import Budget from './Budget'
+import BudgetSnippets from './BudgetSnippets'
+import Transactions from './Transactions'
+
 function Dashboard() {
     return (
-        <>
-        </>
+        <div id="dashboard-container">
+            <div className="dashboard-content-cont" id="budget-breakdown-container">
+                <Budget />
+            </div>
+
+            <div className="dashboard-content-cont" id="budget-snippets-container">
+                <BudgetSnippets />
+            </div>
+
+            <div className="dashboard-content-cont" id="transactions-container">
+                <Transactions />    
+            </div>
+        </div>
     )
 }
 
