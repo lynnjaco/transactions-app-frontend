3c2508807244f3ca5de32ee7871fc60002d37a92
lynnjaco <jacobs.lynnette@gmail.com>
2024-07-06 12:17:16 -0400

    base transaction component html has necessary elements

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -2,8 +2,16 @@ import './Transaction.css'
 
 function Transaction() {
     return (
-        <>
-        </>
+        <div>
+            <p>Date</p>
+            <p>Description</p>
+            <p>Origin</p>
+            {/* Amount text will change color depending on if it's a debit or credit */}
+            <p>Amount</p> 
+            <img src="" alt="Transaction Category Icon" />
+            <img src="" alt="Delete Transaction Icon"/>
+            <img src="" alt="Edit Transaction Icon"/>
+        </div>
     )
 }
 
