e996834b8fc52913a6474214bbb5c08a1998133a
lynnjaco <jacobs.lynnette@gmail.com>
2024-07-08 01:31:20 -0400

    renders avatars for the list of members in the members component

--- src/Components/Members.jsx
+++ src/Components/Members.jsx
@@ -2,8 +2,19 @@ import './Members.css'
 
 function Members( {members} ) {
     return (
-        <>
-        </>
+        <div id='members-container'>
+            <p id='dashboard-text'>Dashboard</p>
+            <div className='row'>
+                <div className='member-avatar'><p>All</p></div>
+                { members.map( member => (
+                    <div className='col'>
+                        <div className='member-avatar'><p>{ member.memberName[0].toUpperCase()}</p></div>
+                        {/* <p id='member-name-text'>{ member.memberName }</p> */}
+                    </div>
+                ))}
+                <div className='member-avatar' id='add-member-text'><p>+</p></div>
+                </div>
+        </div>
     )
 }
 
